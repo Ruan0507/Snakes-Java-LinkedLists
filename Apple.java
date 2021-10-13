42a0287932fe5eb082b0b6dbc0b41aa432deea76
Ruan Small <small.ruan@gmail.com>
2021-10-13 18:06:15 +0200

    Cleaned some unneccessary importing of libraries.

--- Apple.java
+++ Apple.java
@@ -2,33 +2,9 @@
 /**
  * Write a description of class Apple here.
  * 
- * @author (your name) 
+ * @author (Ruan Small) 
  * @version (a version number or a date)
  */
-
-import javax.swing.*;
-import java.awt.*;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.Container;
-import java.awt.Color;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.BorderFactory;
-
-import javax.swing.Timer;
-
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.event.KeyListener;
-import javax.swing.Timer;
-import javax.swing.*;
 
 public class Apple
 {
